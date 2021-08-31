007944df3761e8f08a4d1d711a48d737afe312df
mihanik2828 <mihanik2828@yandex.ru>
2021-08-31 17:25:41 +0300

    load

---
+++ Server/Server/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // устанавливаем для сокета локальную конечную точку
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+
+            // создаем сокет Tcp/Ip
+            Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // назначаем сокет локальной конечной точке и слушаем входящие сокеты
+            try
+            {
+                sListener.Bind(ipEndPoint);
+                sListener.Listen(10);
+
+                // Слушаем соединение
+                while (true)
+                {
+                    Console.WriteLine("Waiting for connect", ipEndPoint);
+
+
+                    Socket handler = sListener.Accept();
+                    string data = null;
+
+                    // Увидели клиента, который к нам подключился
+
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+
+                    data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+
+                    // Вывод данных в консоль
+                    Console.Write("Getted message: " + data + "\n\n");
+
+                    // Отправка ответа клиенту
+                    string reply = "Get contact";
+                    byte[] msg = Encoding.UTF8.GetBytes(reply);
+                    handler.Send(msg);
+
+                    if (data.IndexOf("byebye") > -1)
+                    {
+                        Console.WriteLine("Server has copmleted connetion with client");
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ client/client/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            again:
+                int port = 11000;
+                // Буфер для входящих данных
+                byte[] bytes = new byte[1024];
+
+                //соединяемся с удаленным устройством
+                //устанавливаем удаленную точку для сокета
+
+                IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+                IPAddress ipAddr = ipHost.AddressList[0];
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port);
+
+                Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                //соединяем сокет с удаленной точкой
+                sender.Connect(ipEndPoint);
+
+                Console.WriteLine("Enter message (byebye for the loss connection)");
+                string message = Console.ReadLine();
+
+                Console.WriteLine("Socket conecting... ", sender.RemoteEndPoint.ToString());
+                byte[] msg = Encoding.UTF8.GetBytes(message);
+
+                //отправляем данные через сокет
+                int bytesSent = sender.Send(msg);
+
+                //получаем ответ от сервера
+                int bytesRec = sender.Receive(bytes);
+
+                Console.WriteLine("\nServer response : {0}\n\n", Encoding.UTF8.GetString(bytes, 0, bytesRec));
+
+                //отправляемся на метку для неоднократного прогона участка программы
+                if (message.IndexOf("byebye") == -1)
+                    goto again;
+
+                //oсвобождаем сокет
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+
+    }
+}
